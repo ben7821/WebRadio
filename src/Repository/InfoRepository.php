dcfdf104255e776f0ef1cc1252cc2662da6d4a57
ben7821 <toulatbenjamin@gmail.com>
2023-12-14 15:12:50 +0100

    Init 2h

--- src/Repository/InfoRepository.php
+++ src/Repository/InfoRepository.php
@@ -2,27 +2,27 @@
 
 namespace App\Repository;
 
-use App\Entity\Inscription;
+use App\Entity\Info;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Inscription>
+ * @extends ServiceEntityRepository<Info>
  *
- * @method Inscription|null find($id, $lockMode = null, $lockVersion = null)
- * @method Inscription|null findOneBy(array $criteria, array $orderBy = null)
- * @method Inscription[]    findAll()
- * @method Inscription[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Info|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Info|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Info[]    findAll()
+ * @method Info[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class InscriptionRepository extends ServiceEntityRepository
+class InfoRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Inscription::class);
+        parent::__construct($registry, Info::class);
     }
 
 //    /**
-//     * @return Inscription[] Returns an array of Inscription objects
+//     * @return Info[] Returns an array of Info objects
 //     */
 //    public function findByExampleField($value): array
 //    {
@@ -36,7 +36,7 @@ class InscriptionRepository extends ServiceEntityRepository
 //        ;
 //    }
 
-//    public function findOneBySomeField($value): ?Inscription
+//    public function findOneBySomeField($value): ?Info
 //    {
 //        return $this->createQueryBuilder('i')
 //            ->andWhere('i.exampleField = :val')
