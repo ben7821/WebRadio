258b82d963523bcd13130e1b7249ee97b0bea3a0
ben7821 <toulatbenjamin@gmail.com>
2023-12-18 14:27:36 +0100

    Auth,fileSystem

--- src/Controller/CreationController.php
+++ src/Controller/CreationController.php
@@ -3,24 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 use App\Entity\Emission;
 use App\Entity\Audio;
+use App\Form\EmissionType;
+use App\Form\AudioType;
 
 class CreationController extends AbstractController
 {
     #[Route('/creation', name: 'app_creation')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $emission = $this->getDoctrine()->getRepository(Emission::class)->findAll();
-        $audio = $this->getDoctrine()->getRepository(Audio::class)->findAll();
+        $emission = new Emission();
+        $audio = new Audio();
+
+        $formEmission = $this->createForm(EmissionType::class, $emission);
+        $formAudio = $this->createForm(AudioType::class, $audio);
+
+        $formEmission->handleRequest($request);
+        $formAudio->handleRequest($request);
+
+        if ($formEmission->isSubmitted() && $formEmission->isValid()) {
             
-        return $this->render('creation/index.html.twig', [
-            'controller_name' => 'CreationController',
-            'emission' => $emission,
-            'audio' => $audio,
-        ]);
+        }
     }
 }
