d861a3829e5c927904f9dd7715eb7e0aca48e011
ben7821 <toulatbenjamin@gmail.com>
2023-12-18 15:02:52 +0100

    ....

--- src/Controller/CreationController.php
+++ src/Controller/CreationController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 use App\Entity\Emission;
@@ -14,13 +15,12 @@ use App\Entity\Audio;
 class CreationController extends AbstractController
 {
     #[Route('/creation', name: 'app_creation')]
-    public function index(Request $request): Response
+    public function index(EntityManagerInterface $em): Response
     {
 
 
-        $emission = $container->get('doctrine')->getRepository(Emission::class)->findAll();
-
-        $audio = $doctrine->getRepository(Audio::class)->findAll();
+        $emission = $em->getRepository(Emission::class)->findAll();
+        $audio = $em->getRepository(Audio::class)->findAll();
 
         return $this->render('creation/index.html.twig', [
             'controller_name' => 'CreationController',
