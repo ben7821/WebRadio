6e0479e682c8854fa19e1b84e4fc0193a198692f
ben7821 <toulatbenjamin@gmail.com>
2023-12-18 14:46:26 +0100

    emm,pod

--- src/Controller/CreationController.php
+++ src/Controller/CreationController.php
@@ -10,25 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Emission;
 use App\Entity\Audio;
-use App\Form\EmissionType;
-use App\Form\AudioType;
 
 class CreationController extends AbstractController
 {
     #[Route('/creation', name: 'app_creation')]
     public function index(Request $request): Response
     {
-        $emission = new Emission();
-        $audio = new Audio();
 
-        $formEmission = $this->createForm(EmissionType::class, $emission);
-        $formAudio = $this->createForm(AudioType::class, $audio);
 
-        $formEmission->handleRequest($request);
-        $formAudio->handleRequest($request);
+        $emission = $doctrine->getRepository(Emission::class)->findAll();
 
-        if ($formEmission->isSubmitted() && $formEmission->isValid()) {
-            
-        }
+        $audio = $doctrine->getRepository(Audio::class)->findAll();
+
+        return $this->render('creation/index.html.twig', [
+            'controller_name' => 'CreationController',
+            'emissions' => $emission,
+            'audios' => $audio,
+        ]);
     }
 }
---
+++ src/Controller/PodcastController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PodcastController extends AbstractController
+{
+    #[Route('/podcast', name: 'app_podcast')]
+    public function index(): Response
+    {
+        return $this->render('podcast/index.html.twig', [
+            'controller_name' => 'PodcastController',
+        ]);
+    }
+
+    #[Route('/podcast', name: 'app_audio')]
+    public function podcast(): Response {
+        return $this->render('podcast/index.html.twig', [
+            'podcasts' => 'podcasts',
+        ]);
+    }
+}
